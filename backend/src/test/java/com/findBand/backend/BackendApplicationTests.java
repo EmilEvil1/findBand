2067d633651f859b007d9c945d600e326e375ba4
dssorokin <dssorokin94@gmail.com>
2021-08-27 23:43:16 +0300

    the base of backend

---
+++ backend/src/test/java/com/findBand/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findBand.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
