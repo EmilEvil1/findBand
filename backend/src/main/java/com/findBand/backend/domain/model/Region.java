956a76319274e5154bcb39498317e8f8cf20f7cb
daniilSorokin94 <d.sorokin@onvoya.com>
2021-11-14 16:29:19 +0300

    store regions in redis

--- backend/src/main/java/com/findBand/backend/domain/model/Region.java
+++ backend/src/main/java/com/findBand/backend/domain/model/Region.java
@@ -1,12 +1,14 @@
 package com.findBand.backend.domain.model;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author dsorokin on 04.11.2021
  */
 
+@RequiredArgsConstructor
 @Data
 public class Region {
-	private String regionName;
+	private final String regionName;
 }
--- backend/src/main/java/com/findBand/backend/domain/port/RegionsPort.java
+++ backend/src/main/java/com/findBand/backend/domain/port/RegionsPort.java
@@ -1,12 +1,11 @@
 package com.findBand.backend.domain.port;
 
-import com.findBand.backend.domain.model.City;
 import com.findBand.backend.domain.model.Region;
 
-import java.util.List;
+import java.util.Set;
 
 public interface RegionsPort {
-	List<Region> findCitiesBySearchKey(String searchKey);
+	Set<Region> findCitiesBySearchKey(String searchKey);
 
-	void saveAllRegions(List<String> regions);
+	Set<Region> findAllRegions();
 }
