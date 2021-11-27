ee694984b99f403de560751b0830305e205be770
dssorokin <dssorokin94@gmail.com>
2021-11-27 15:19:49 +0300

    add MailerPort implementation

--- backend/src/main/java/com/findBand/backend/infra/adapters/common/SpringMailer.java
+++ backend/src/main/java/com/findBand/backend/infra/adapters/common/SpringMailer.java
@@ -1,12 +1,35 @@
 package com.findBand.backend.infra.adapters.common;
 
 import com.findBand.backend.domain.port.MailerPort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import javax.mail.internet.MimeMessage;
+
 @Component
+@Slf4j
 public class SpringMailer implements MailerPort {
+
+    private JavaMailSender mailSender;
+
+    public SpringMailer(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     @Override
     public void sendEmail(String fromEmailAddress, String toEmailAddress, String body) {
+       try {
+           MimeMessage mimeMessage = mailSender.createMimeMessage();
+           MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+           mimeMessageHelper.setTo(toEmailAddress);
+           mimeMessageHelper.setFrom("dssorokin94@gmail.com");
+           mimeMessageHelper.setText(body);
 
+           mailSender.send(mimeMessage);
+       } catch (Exception e) {
+           log.error("Error in sending message from: {}, to: {}", fromEmailAddress, toEmailAddress, e);
+       }
     }
 }
