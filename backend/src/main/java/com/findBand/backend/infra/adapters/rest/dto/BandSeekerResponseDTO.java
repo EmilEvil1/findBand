ff4c473fd0967e348b32f785589a52eb65b7c850
dssorokin <dssorokin94@gmail.com>
2022-05-27 10:57:30 +0300

    fix deploy code in maven.yml

--- backend/src/main/java/com/findBand/backend/infra/adapters/rest/dto/BandSeekerResponseDTO.java
+++ backend/src/main/java/com/findBand/backend/infra/adapters/rest/dto/BandSeekerResponseDTO.java
@@ -3,9 +3,11 @@ package com.findBand.backend.infra.adapters.rest.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Builder
 @Data
-public class BandSeekerResponseDTO {
+public class BandSeekerResponseDTO implements Serializable {
     private String name;
     private String phone;
     private String email;
