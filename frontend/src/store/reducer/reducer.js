95cf0659c4684613f9af1c87f317b3db287114e5
evilEmil <ev@hawkingbros.com>
2022-06-06 18:04:27 +0300

    update banner

--- frontend/src/store/reducer/reducer.js
+++ frontend/src/store/reducer/reducer.js
@@ -11,6 +11,12 @@ import photo1  from './../../assets/img/mainSlider/bob-marley.jpg'
 import photo2  from './../../assets/img/mainSlider/beatles.jpg'
 import photo3  from './../../assets/img/mainSlider/bethoven.jpg'
 import photo4  from './../../assets/img/mainSlider/mike.jpg'
+import photo5  from './../../assets/img/mainSlider/sting.jpg'
+import photo6  from './../../assets/img/mainSlider/amy.webp'
+import photo7  from './../../assets/img/mainSlider/elton.jpg'
+import photo8  from './../../assets/img/mainSlider/ozi.jpg'
+import photo9  from './../../assets/img/mainSlider/mik.jpg'
+import photo10  from './../../assets/img/mainSlider/dragons.jpg'
 
 const initialState = {
     info: {},
@@ -36,6 +42,36 @@ const initialState = {
             author: 'Майкл Джексон',
             text: "«Мне нравится создавать музыку, которая понятна всем, от фермера в Ирландии до уборщицы туалетов в Гарлеме»",
             img: photo4
+        },
+        {
+            author: 'Стинг',
+            text: "«Идеальная музыка — это тишина, а музыканты занимаются созданием красивой рамки вокруг этого совершенства»",
+            img: photo5
+        },
+        {
+            author: 'Эми Уайнхаус',
+            text: "«Самая большая ошибка, которую только могут сделать родители, это зачать ребенка под плохую музыку»",
+            img: photo6
+        },
+        {
+            author: 'Элтон Джон',
+            text: "«Музыка обладает исцеляющей силой. На несколько часов она может заставить тебя покинуть твое тело, чтобы ты мог, наконец, взглянуть на себя со стороны»",
+            img: photo7
+        },
+        {
+            author: 'Оззи Осборн',
+            text: "«Все эти разговоры о металле и харде… я не подпишусь ни под единым словом. Рок — это просто хорошая музыка. Его нужно слушать, а не говорить о нем»",
+            img: photo8
+        },
+        {
+            author: 'Мик Джаггер',
+            text: "«Моей матери никогда не нравилось, чем я занимался. Она была бы счастливее, будь я каменщиком»",
+            img: photo9
+        },
+        {
+            author: 'Дэн Рейнолдс, Imagine Dragons',
+            text: "«Я уверен, что люди, творящие музыку, делают ее не потому что хотят, а потому что должны»",
+            img: photo10
         }
     ],
     searchData: null,
