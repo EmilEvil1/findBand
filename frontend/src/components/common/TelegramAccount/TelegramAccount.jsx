ebbba535f73cdbbc385b75eee4076f911b55d48e
evilEmil <ev@hawkingbros.com>
2022-06-16 21:22:07 +0300

    added telegramm btn

--- frontend/src/components/common/TelegramAccount/TelegramAccount.jsx
+++ frontend/src/components/common/TelegramAccount/TelegramAccount.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import TelegramLoginButton from 'react-telegram-login';
 
 const TelegramAccount = () => {
+
+
+    const handleTelegramResponse = response => {
+        console.log(response);
+    };
     return (
-        <script
-            async
-            src="https://telegram.org/js/telegram-widget.js?19"
-            data-telegram-login="FindBandBot"
-            data-size="large"
-            data-radius="10"
-            data-auth-url=""
-            data-request-access="write">
-        </script>
+        <>
+            <TelegramLoginButton dataOnauth={handleTelegramResponse} botName="FindBandBot" />
+            {/*<script*/}
+            {/*    async*/}
+            {/*    src="https://telegram.org/js/telegram-widget.js?19"*/}
+            {/*    data-telegram-login="FindBandBot"*/}
+            {/*    data-size="large"*/}
+            {/*    data-radius="10"*/}
+            {/*    data-auth-url=""*/}
+            {/*    data-request-access="write">*/}
+            {/*</script>*/}
+        </>
+
+
     );
 };
 
