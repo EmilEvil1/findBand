7445397ba3453f314159e259ad5793185a91655c
evilEmil <ev@hawkingbros.com>
2022-06-16 21:48:14 +0300

    completed telegram account flow

--- frontend/src/components/common/TelegramAccount/TelegramAccount.jsx
+++ frontend/src/components/common/TelegramAccount/TelegramAccount.jsx
@@ -3,26 +3,11 @@ import TelegramLoginButton from 'react-telegram-login';
 
 const TelegramAccount = () => {
 
-
     const handleTelegramResponse = response => {
         console.log(response);
-    };
-    return (
-        <>
-            <TelegramLoginButton dataOnauth={handleTelegramResponse} botName="FindBandBot" />
-            {/*<script*/}
-            {/*    async*/}
-            {/*    src="https://telegram.org/js/telegram-widget.js?19"*/}
-            {/*    data-telegram-login="FindBandBot"*/}
-            {/*    data-size="large"*/}
-            {/*    data-radius="10"*/}
-            {/*    data-auth-url=""*/}
-            {/*    data-request-access="write">*/}
-            {/*</script>*/}
-        </>
+    }
 
-
-    );
+    return <TelegramLoginButton dataOnauth={handleTelegramResponse} botName="FindBandBot" />
 };
 
 export default TelegramAccount;
