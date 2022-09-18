0a317314cccdc73ddc62c01cf4ec125af9f936e0
daniilsorokin <dssorokin94@gmail.com>
2022-09-18 21:46:51 +0300

    FB-110: поменял get на post

--- backend/src/main/java/com/findBand/backend/infra/adapters/rest/controller/SearchController.java
+++ backend/src/main/java/com/findBand/backend/infra/adapters/rest/controller/SearchController.java
@@ -13,10 +13,7 @@ import com.findBand.backend.infra.adapters.rest.dto.search.SearchForMemberReques
 import com.findBand.backend.infra.adapters.rest.dto.search.VacancyDTO;
 import com.findBand.backend.infra.common.rest.BaseController;
 import com.findBand.backend.infra.common.rest.DataResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -32,7 +29,7 @@ public class SearchController extends BaseController {
         this.useCasePublisher = useCasePublisher;
     }
 
-    @PostMapping(value = "searchForMembers")
+    @GetMapping(value = "searchForMembers")
     public DataResponse<BandSeekerDTO> doSearchForMember(@RequestBody SearchForMemberRequestDTO request) {
         SearchForMember searchForMember = new SearchForMember(Collections.singletonList(request.getInstrumentId()), request.getRegionId());
         List<UserDomain> bandSeekers = useCasePublisher.publish(List.class, searchForMember);
